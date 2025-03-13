1327270c2439697067d13a861f13f9488a34b11c
Manowcrm <abdirahmancrm@outlook.com>
2025-03-13 11:29:36 +0000

    Add login functionality and update styles

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 // Create express app
 var app = express();
 
 // Add static files location
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "..", "static")));
 
 // Use the Pug templating engine
 app.set('view engine', 'pug');
@@ -13,33 +14,15 @@ app.set('views', path.join(__dirname, 'services', 'views'));
 
 console.log("Views directory set to:", path.join(__dirname, 'services', 'views'));
 
+// Middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
     res.render("index", { title: "Home" });
-});
-
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
-});
-
-// Create a route for /goodbye
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>
-app.get("/hello/:name", function(req, res) {
-    console.log(req.params);
-    res.send("Hello " + req.params.name);
 });
 
 // Create a route for /signup
@@ -60,6 +43,29 @@ app.get("/features", function(req, res) {
 // Create a route for /how_it_works
 app.get("/how_it_works", function(req, res) {
     res.render("how_it_works", { title: "How It Works" });
+});
+
+// Create a route for /login
+app.get("/login", function(req, res) {
+    res.render("login", { title: "Log In" });
+});
+
+// Handle login process
+app.post("/login_process", function(req, res) {
+    const { email, password } = req.body;
+
+    // Dummy user data for demonstration purposes
+    const users = [
+        { email: "user@example.com", password: "password123" }
+    ];
+
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if (user) {
+        res.send("Welcome back!");
+    } else {
+        res.render("login", { title: "Log In", error: "Don't recognize credentials, please create an account." });
+    }
 });
 
 // Start server on port 3000
